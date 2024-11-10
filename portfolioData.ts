208b29cd24e05a7223b7f063676a16304307fbbd
Shailesh Acharya <me@acharyashailesh.com.np>
2024-11-10 09:36:17 +0545

    fix schema

--- portfolioData.ts
+++ portfolioData.ts
@@ -19,27 +19,71 @@ export const portfolioData = {
         "postalCode": "32907",
         "addressCountry": "NP"
     },
-    "alumniOf": {
-        "@type": "CollegeOrUniversity",
-        "name": "Butwal Multiple Campus",
-        "sameAs": "https://bumc.edu.np/"
+    "hasCredential": {
+        "@type": "EducationalOccupationalCredential",
+        "credentialCategory": "master's degree",
+        "educationalLevel": "graduate"
     },
-    "knowsAbout": ["AWS/Azure", "Git", "HTML/CSS", "Python", "Machine Learning", "Cloud Computing", "HUGO", "Next.js", "Tailwind CSS", "SEO", "WordPress"],
+    "alumniOf": [
+        {
+            "@type": "EducationalOrganization",
+            "name": "Tribhuvan University",
+            "sameAs": "https://cdcsit.edu.np/",
+            "award": "MSc. Computer Science and Information Technology",
+            "startDate": "2023",
+            "endDate": "Present"
+        },
+        {
+            "@type": "EducationalOrganization",
+            "name": "Butwal Multiple Campus",
+            "sameAs": "https://bumc.edu.np/",
+            "award": "BSc. Computer Science and Information Technology",
+            "startDate": "2016",
+            "endDate": "2020"
+        }
+    ],
+    "knowsAbout": [
+        "AWS/Azure",
+        "Git",
+        "HTML/CSS",
+        "Python",
+        "Machine Learning",
+        "Cloud Computing",
+        "HUGO",
+        "Next.js",
+        "Tailwind CSS",
+        "SEO",
+        "WordPress"
+    ],
+    "hasOccupation": [
+        {
+            "@type": "Occupation",
+            "name": "Full Stack Web Developer",
+            "occupationalCategory": "15-1254.00",
+            "skills": "Web Development, Cloud Architecture, Machine Learning",
+            "estimatedSalary": {
+                "@type": "MonetaryAmount",
+                "currency": "NPR",
+                "value": {
+                    "@type": "QuantitativeValue",
+                    "minValue": 50000,
+                    "maxValue": 150000,
+                    "unitText": "MONTH"
+                }
+            }
+        }
+    ],
     "makesOffer": [
         {
             "@type": "Offer",
             "itemOffered": {
                 "@type": "Service",
                 "name": "Full Stack Web Development",
-                "description": "Design and development of web applications"
-            }
-        },
-        {
-            "@type": "Offer",
-            "itemOffered": {
-                "@type": "Service",
-                "name": "Mobile App Development",
-                "description": "Creation of cross-platform mobile applications"
+                "description": "Design and development of web applications",
+                "provider": {
+                    "@type": "Person",
+                    "name": "Shailesh Acharya"
+                }
             }
         },
         {
@@ -47,112 +91,96 @@ export const portfolioData = {
             "itemOffered": {
                 "@type": "Service",
                 "name": "Cloud Architecture Consulting",
-                "description": "Design and implementation of cloud-based solutions"
+                "description": "Design and implementation of cloud-based solutions",
+                "provider": {
+                    "@type": "Person",
+                    "name": "Shailesh Acharya"
+                }
             }
         }
     ],
-    "resume": {
-        "@type": "CreativeWork",
-        "name": "Shailesh Acharya Resume",
-        "url": "https://acharyashailesh.com.np/shailesh_resume.pdf",
-        "about": {
-            "@type": "ItemList",
-            "itemListElement": [
-                {
-                    "@type": "ListItem",
-                    "position": 1,
-                    "item": {
-                        "@type": "Course",
-                        "name": "Work Experience",
-                        "description": "Freelenacer, Web Developer, Student (2020-Present)",
-                        "provider": {
-                            "@type": "Organization",
-                            "name": "Various Organizations"
-                        },
-                        "hasCourseInstance": {
-                            "@type": "CourseInstance",
-                            "courseMode": "full-time",
-                            "offers": {
-                                "@type": "Offer",
-                                "category": "Professional Experience"
-                            }
-                        }
-                    }
-                },
-                {
-                    "@type": "ListItem",
-                    "position": 2,
-                    "item": {
-                        "@type": "Course",
-                        "name": "Education",
-                        "description": "Master's in Computer Science, Tribhuvan University, CDCSIT (2023-Present)",
-                        "provider": {
-                            "@type": "Organization",
-                            "name": "Tribhuvan University",
-                            "sameAs": "https://cdcsit.edu.np/"
-                        },
-                        "hasCourseInstance": {
-                            "@type": "CourseInstance",
-                            "courseMode": "full-time",
-                            "instructor": {
-                                "@type": "Person",
-                                "name": "Department of Computer Science"
-                            },
-                            "offers": {
-                                "@type": "Offer",
-                                "category": "Master's Degree",
-                                "priceSpecification": {
-                                    "@type": "PriceSpecification",
-                                    "price": "250000",
-                                    "priceCurrency": "NPR"
-                                }
-                            }
-                        }
-                    }
-                },
-                {
-                    "@type": "ListItem",
-                    "position": 3,
-                    "item": {
-                        "@type": "Course",
-                        "name": "Skills",
-                        "description": "Next.js, Tailwind CSS, HUGO, WordPress, Python, Machine Learning, Aws/Azure, SEO",
-                        "provider": {
-                            "@type": "Organization",
-                            "name": "Self-Learned & Professional Experience"
-                        },
-                        "hasCourseInstance": {
-                            "@type": "CourseInstance",
-                            "courseMode": "online",
-                            "offers": {
-                                "@type": "Offer",
-                                "category": "Professional Skills"
-                            }
-                        }
-                    }
-                },
-                {
-                    "@type": "ListItem",
-                    "position": 4,
-                    "item": {
-                        "@type": "Course",
-                        "name": "Projects",
-                        "description": "Portfolio Website, Arduino/Raspberry Pi-based projects, Machine Learning Projects",
-                        "provider": {
-                            "@type": "Organization",
-                            "name": "Personal & Academic Projects"
-                        },
-                        "hasCourseInstance": {
-                            "@type": "CourseInstance",
-                            "courseMode": "hybrid",
-                            "offers": {
-                                "@type": "Offer",
-                                "category": "Project Portfolio"
-                            }
-                        }
-                    }
-                }
-            ]
+    "workExperience": [
+        {
+            "@type": "WorkPosition",
+            "name": "Volunteer",
+            "worksFor": {
+                "@type": "Organization",
+                "name": "Python Nepal"
+            },
+            "startDate": "2022",
+            "endDate": "2023",
+            "description": "Discuss, share, and learn about Python use cases in the AI, deep learning, and machine learning fields."
+        },
+        {
+            "@type": "WorkPosition",
+            "name": "Data Expedition",
+            "worksFor": {
+                "@type": "Organization",
+                "name": "oknp.org"
+            },
+            "startDate": "2021",
+            "endDate": "2021",
+            "description": "Involved in research, advocacy, training, workshops, and developing tools related to open knowledge."
+        },
+        {
+            "@type": "WorkPosition",
+            "name": "Intern",
+            "worksFor": {
+                "@type": "Organization",
+                "name": "WorldLink"
+            },
+            "startDate": "2019",
+            "endDate": "2020",
+            "description": "Worked on a team responsible for providing Tech Support to clients regarding Internet connection and IPTV."
         }
+    ],
+    "hasSkill": {
+        "@type": "DefinedTerm",
+        "termCode": "Technical Skills",
+        "skills": [
+            {
+                "@type": "DefinedTerm",
+                "termCode": "Web Development",
+                "proficiencyLevel": "Expert",
+                "skills": ["Next.js", "HTML/CSS", "JavaScript", "Tailwind CSS"]
+            },
+            {
+                "@type": "DefinedTerm",
+                "termCode": "Cloud Computing",
+                "proficiencyLevel": "Advanced",
+                "skills": ["AWS", "Azure"]
+            },
+            {
+                "@type": "DefinedTerm",
+                "termCode": "Programming",
+                "proficiencyLevel": "Expert",
+                "skills": ["Python", "JavaScript", "TypeScript"]
+            }
+        ]
+    },
+    "mainEntityOfPage": {
+        "@type": "WebPage",
+        "name": "Shailesh Acharya - Portfolio",
+        "description": "Personal portfolio website of Shailesh Acharya, a Full Stack Developer and Master's Student",
+        "significantLink": [
+            {
+                "@type": "WebPage",
+                "name": "Portfolio Website",
+                "url": "https://github.com/iamshaileshacharya/My-portfolio",
+                "description": "A personal portfolio website built with Next.js, Framer Motion and Tailwind CSS."
+            },
+            {
+                "@type": "WebPage",
+                "name": "Email Spam Detection",
+                "url": "https://github.com/iamshaileshacharya/python-spam-detection",
+                "description": "A machine learning model with GUI to detect spam emails using Python."
+            },
+            {
+                "@type": "WebPage",
+                "name": "Smart Car Parking System",
+                "url": "https://github.com/yourusername/ai-chatbot",
+                "description": "An intelligent car parking monitoring system using Arduino and Raspberry Pi."
+            }
+        ]
     }
 }
